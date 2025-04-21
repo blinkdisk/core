2130baa91f8e36d87943ef526a69c7acf158d0ee
Paul Koeck <paul@koeck.dev>
2025-04-21 15:45:55 +0200

    refactor: delete unused files from upstream repo

---
+++ tools/fork/delete-unused.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf .chglog/
+rm -rf .github/
+rm -rf app/
+rm -rf BUILD.md
+rm -rf GOVERNANCE.md
+rm -rf CODE_OF_CONDUCT.md
+rm -rf build-status.md
+rm -rf build_architecture.svg
+rm -rf icons/
+rm -rf site/
+rm -rf .snyk
